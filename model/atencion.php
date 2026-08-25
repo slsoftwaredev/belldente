aa022e8298e654fb4dfd3d10d37b87b1a97f2777
K1050155850 <lemastalin344@gmail.com>
2026-08-24 23:47:43 -0500

    Se agrega un null porque estamos enviando un JSON a mysql, se corrigen los módulos para que funciones

--- model/atencion.php
+++ model/atencion.php
@@ -13,33 +13,33 @@ class Atencion
     public function crear($id_cita, $id_usuario){
         $id_cita = intval($id_cita);
         $id_usuario = intval($id_usuario);
-        $sql = "CALL sp_atencion('crear',0,0,'$id_usuario','$id_cita')";
+        $sql = "CALL sp_atencion('crear',0,0,'$id_usuario','$id_cita',NULL)";
         return ejecutarConsultaSimpleFilaAssoc($sql);
     }
     // Método para listar los antecedentes de un paciente
     public function listarAntecedentes(){
-    $sql = "CALL sp_atencion('listar_antecedentes',NULL,NULL,NULL,NULL)";
+    $sql = "CALL sp_atencion('listar_antecedentes',NULL,NULL,NULL,NULL,NULL)";
     return ejecutarConsulta($sql);
     }
     //Método para listar el examen estomatognático de un paciente
     public function listarEstomatognatico(){
-    $sql = "CALL sp_atencion('listar_estomatognatico',NULL,NULL,NULL,NULL)";
+    $sql = "CALL sp_atencion('listar_estomatognatico',NULL,NULL,NULL,NULL,NULL)";
     return ejecutarConsulta($sql);
     }
     //Método para listar los indicadores de salud bucal de un paciente
     public function listarIndicadores(){
-    $sql = "CALL sp_atencion('listar_indicadores',NULL,NULL,NULL,NULL)";
+    $sql = "CALL sp_atencion('listar_indicadores',NULL,NULL,NULL,NULL,NULL)";
     return ejecutarConsulta($sql);
     }
     //Combo CIE-10 - Procedimientos o protocolos de atención
     public function comboCIE10(){
-    $sql = "CALL sp_atencion('combo_cie10',0,0,0,0)";
+    $sql = "CALL sp_atencion('combo_cie10',0,0,0,0,NULL)";
     return ejecutarConsultaSP($sql);
 
     }
     // Combo de tratamientos
     public function comboTratamientos(){
-    $sql = "CALL sp_atencion('combo_tratamientos',0,0,0,0)";
+    $sql = "CALL sp_atencion('combo_tratamientos',0,0,0,0,NULL)";
     return ejecutarConsultaSP($sql);
 
     }
